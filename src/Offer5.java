e8d9504769de22317610182cd61458a8917cc5f0
Zhu <964237881@qq.com>
2019-07-08 21:27:36 +0800

    Offer5

--- src/Offer5.java
+++ src/Offer5.java
@@ -1,49 +1,36 @@
 public class Offer5 {
 //从后往前赋值，降低时间复杂度
-    public static void replaceBlank(char[] arr){
-        int originalLength = 0;
+    public static String replaceBlank(StringBuffer str){
+        if(str == null)
+            return null;
+        String string = str.toString();
         int numberOfBlank = 0;
-        for (int i = 0; i < arr.length; ++i){
-            if(i == 12)
-                break;
-            ++originalLength;
-            if(arr[i] == ' ')
+        int originalLength = str.length();
+        for (int i = 0; i < originalLength; ++i){
+            if(str.charAt(i) == ' ')
                 ++numberOfBlank;
         }
 
         int newLength = originalLength + 2 * numberOfBlank;
+        str.setLength(newLength);
         int indexOfOriginal = originalLength - 1;
         int indexOfNew = newLength - 1;
         while (indexOfNew != indexOfOriginal){
-            if(arr[indexOfOriginal] == ' '){
-                arr[indexOfNew--] = '0';
-                arr[indexOfNew--] = '2';
-                arr[indexOfNew--] = '%';
+            if(string.charAt(indexOfOriginal) == ' '){
+                str.setCharAt(indexOfNew--, '0');
+                str.setCharAt(indexOfNew--, '2');
+                str.setCharAt(indexOfNew--, '%');
                 --indexOfOriginal;
             }else {
-                arr[indexOfNew--] = arr[indexOfOriginal--];
+                str.setCharAt(indexOfNew--, string.charAt(indexOfOriginal--));
             }
         }
+        return String.valueOf(str);
     }
 
     public static void main(String[] args) {
-        char[] test = new char[20];
-        test[0] = 'w';
-        test[1] = 'e';
-        test[2] = ' ';
-        test[3] = 'a';
-        test[4] = 'r';
-        test[5] = 'e';
-        test[6] = ' ';
-        test[7] = 'h';
-        test[8] = 'a';
-        test[9] = 'p';
-        test[10] = 'p';
-        test[11] = 'y';
-        replaceBlank(test);
-        for (Character character: test){
-            System.out.print(character);
-        }
+        StringBuffer test = new StringBuffer("we are happy");
+        System.out.print(replaceBlank(test));
         System.out.println();
     }
 }
